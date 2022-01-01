e0a81805157fabf36882695c6bd3d85e34f62504
Frank Albanese <albanesefc9@gmail.com>
2022-01-01 01:41:18 -0500

    Functional minter on login

--- front_end/dapp/routes.py
+++ front_end/dapp/routes.py
@@ -2,9 +2,10 @@ from dapp import app
 from flask import render_template, redirect
 import json
 
+with open('/Users/eliafrank/Desktop/Projects/web3/nft_minter/front_end/dapp/static/ERC721PresetMinterPauserAutoId.json', 'r') as f:
+    abi = json.load(f)['abi']
+
 @app.route('/')
 def home_page():
-    with open('static/ERC721PresetMinterPauserAutoId.json', 'r') as f:
-        abi = json.load(f)
-        
-    return render_template('home.html')
+    nft_abi = abi
+    return render_template('home.html', nft_abi=nft_abi)
